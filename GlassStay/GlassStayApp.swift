a382a0085e5e7259e6c1495669d32631b6e54c51
Anupam Kumar Lal <anupam.lal@ril.com>
2024-03-17 00:28:35 +0530

    initial commit

---
+++ GlassStay/GlassStayApp.swift
@@ -0,0 +1,17 @@
+//
+//  GlassStayApp.swift
+//  GlassStay
+//
+//  Created by Anupam Lal on 17/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GlassStayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
